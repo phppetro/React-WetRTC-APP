d4408c4b93deccdfb414f4d444c02a4276407d79
webdozen <webdozen@gmail.com>
2018-07-21 10:13:34 +1100

    Working WS Server

--- commit.sh
+++ commit.sh
@@ -4,9 +4,9 @@ Y=2018
 M=7
 D=21
 i=13
-comment="Initial commit"
+comment="Working WS Server"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
-git add commit.md -f
+# git add commit.md -f
 git commit --date="$Y-$M-$D 10:$i:34" -m "$comment"
