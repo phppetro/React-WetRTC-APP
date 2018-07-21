02856c04f1680ebe45af530f20e8b51cdc0814a3
webdozen <webdozen@gmail.com>
2018-07-22 10:13:34 +1100

    Small changes

--- commit.sh
+++ commit.sh
@@ -4,7 +4,7 @@ Y=2018
 M=7
 D=22
 i=13
-comment="Added user selected component."
+comment="Small changes"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
