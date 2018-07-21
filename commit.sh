8c8d613b95152e5b539812c184d2b70b4937dfae
webdozen <webdozen@gmail.com>
2018-07-22 10:13:34 +1100

    Added name input modal.

--- commit.sh
+++ commit.sh
@@ -4,7 +4,7 @@ Y=2018
 M=7
 D=22
 i=13
-comment="Add video element to React App"
+comment="Added name input modal."
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
