3e7da39b1adae8585c6a91e5f0370f7b9aedea31
webdozen <webdozen@gmail.com>
2018-07-22 10:13:34 +1100

    Add video element to React App

--- commit.sh
+++ commit.sh
@@ -2,9 +2,9 @@
 
 Y=2018
 M=7
-D=21
+D=22
 i=13
-comment="Working WS Server"
+comment="Add video element to React App"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
