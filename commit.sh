faa17a7e9321bd497f848799353e80de8540078e
ruslanpopelyshyn <ruslan.popelyshyn@hotmail.com>
2018-07-23 10:20:34 +1100

    Added algorithm for sigining app

--- commit.sh
+++ commit.sh
@@ -3,8 +3,8 @@
 Y=2018
 M=7
 D=23
-i=13
-comment="Tracks are now added to Peer. "
+i=20
+comment="Added algorithm for sigining app "
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
