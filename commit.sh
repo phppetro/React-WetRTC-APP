4450e44e9c0bdae386c5e1b9aab437a9e94ae6bc
ruslanpopelyshyn <ruslan.popelyshyn@hotmail.com>
2018-07-23 10:13:34 +1100

    Front end change more color

--- commit.sh
+++ commit.sh
@@ -2,9 +2,9 @@
 
 Y=2018
 M=7
-D=22
+D=23
 i=13
-comment="Small changes"
+comment="Front end change more color "
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
