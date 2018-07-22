839e0b0745acd8dc43d09c0f7aadc2900251f6db
ruslanpopelyshyn <ruslan.popelyshyn@hotmail.com>
2018-07-23 10:20:34 +1100

    UserSelector now takes data from the server

--- commit.sh
+++ commit.sh
@@ -4,7 +4,7 @@ Y=2018
 M=7
 D=23
 i=20
-comment="Added algorithm for sigining app "
+comment="UserSelector now takes data from the server"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
