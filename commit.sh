beac368de89ce9cf2daff78081062e4ec871ec9a
ruslanpopelyshyn <ruslan.popelyshyn@hotmail.com>
2018-07-23 10:13:34 +1100

    Tracks are now added to Peer.

--- commit.sh
+++ commit.sh
@@ -4,7 +4,7 @@ Y=2018
 M=7
 D=23
 i=13
-comment="Front end change more color "
+comment="Tracks are now added to Peer. "
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
