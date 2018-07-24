f2f787c9f02cd14dd231b2fbec8e87418fd5aacf
ruslanpopelyshyn <ruslan.popelyshyn@hotmail.com>
2018-07-25 10:21:34 +1100

    Added Procfile for heroku deployment

--- commit.sh
+++ commit.sh
@@ -4,9 +4,10 @@ Y=2018
 M=7
 D=25
 i=21
-comment="WORKING PEER TO PEER CONNECTION"
+comment="Added Procfile for heroku deployment"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
 # git add commit.md -f
+git add .
 git commit --date="$Y-$M-$D 10:$i:34" -m "$comment"
