268cba9330a560d98d2c43268744d4d6bd32e393
ruslanpopelyshyn <ruslan.popelyshyn@hotmail.com>
2018-07-25 10:21:34 +1100

    Both the peers now get remote tracks

--- commit.sh
+++ commit.sh
@@ -4,7 +4,7 @@ Y=2018
 M=7
 D=25
 i=21
-comment="Revamped redux"
+comment="Both the peers now get remote tracks"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
