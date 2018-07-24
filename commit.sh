2a980a4b612da2e807041a2c245992cc71c5bb20
ruslanpopelyshyn <ruslan.popelyshyn@hotmail.com>
2018-07-25 10:21:34 +1100

    Revamped redux

--- commit.sh
+++ commit.sh
@@ -4,10 +4,11 @@ Y=2018
 M=7
 D=25
 i=21
-comment="Added Procfile for heroku deployment"
+comment="Revamped redux"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
 # git add commit.md -f
 git add .
 git commit --date="$Y-$M-$D 10:$i:34" -m "$comment"
+git push
