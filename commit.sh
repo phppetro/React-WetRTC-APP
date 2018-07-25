37a646ad8eabd35679084b1d0dbcc87ec809d6ab
ruslanpopelyshyn <ruslan.popelyshyn@hotmail.com>
2018-07-26 10:21:34 +1100

    List will be updated when a socket leaves the room

--- commit.sh
+++ commit.sh
@@ -4,7 +4,7 @@ Y=2018
 M=7
 D=26
 i=21
-comment="Added HTTP server for Heroku deployment"
+comment="List will be updated when a socket leaves the room"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
