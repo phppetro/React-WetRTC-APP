faec9a020103e35fdec79a609ecfd8f2f3650876
ruslanpopelyshyn <ruslan.popelyshyn@hotmail.com>
2018-07-26 10:21:34 +1100

    Video calling works

--- commit.sh
+++ commit.sh
@@ -2,9 +2,9 @@
 
 Y=2018
 M=7
-D=25
+D=26
 i=21
-comment="Both the peers now get remote tracks"
+comment="Video calling works"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
